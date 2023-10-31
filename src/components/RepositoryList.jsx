74c3ceafca3636b8c4dbce1a5433b8c5fba112ff
Nvutu-Eladio <Eladio2019@hotmail.com>
2023-10-31 15:30:10 -0300

    Listando os repositorios

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -4,13 +4,6 @@ import '../styles/repositories.scss'
 
 //https://api.github.com/users/Nvutu-Eladio/repos
 
-const repository = {
-    name: 'unform',
-    description: 'Forms in React',
-    link: 'https://github.com/unform/unform'
-}
-
-
 export function RepositoryList() {
 
     const [repositories, setRepositories] = useState([])
@@ -27,11 +20,11 @@ export function RepositoryList() {
             <h1>Lista de repositórios</h1>
 
             <ul>
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
-            </ul>
+                {repositories.map(repository => {
+                    return <RepositoryItem key={repository.name} repository={repository} />
+                })}
+            </ul>   
+                
         </section>
     )
 }
